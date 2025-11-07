67c059689ee58071321b96558f55ba098ca5b13b
Tra Nguyen <tra.nguyen@gmail.com>
2025-11-06 19:23:07 -0800

    set up mongodb

---
+++ packages/server/src/index.ts
@@ -0,0 +1,19 @@
+// src/index.ts
+import express, { Request, Response } from "express";
+import { connect } from "./services/mongo";
+
+connect("splitroom");
+
+const app = express();
+const port = process.env.PORT || 3000;
+const staticDir = process.env.STATIC || "public";
+
+app.use(express.static(staticDir));
+
+app.get("/hello", (req: Request, res: Response) => {
+    res.send("Hello, World");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
